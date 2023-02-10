ab59c0af86ee2a6ddaa726249249ee417c544615
shanart <shanin.x@gmail.com>
2023-02-10 15:24:54 +0200

    016

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -3,15 +3,14 @@ import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
 import { Course } from "../model/course";
 import { FormBuilder, Validators, FormGroup } from "@angular/forms";
 import * as moment from 'moment';
-import { catchError } from 'rxjs/operators';
-import { throwError } from 'rxjs';
 import { CoursesService } from '../services/courses.service';
 import { LoadingService } from '../loading/loading.service';
 
 @Component({
     selector: 'course-dialog',
     templateUrl: './course-dialog.component.html',
-    styleUrls: ['./course-dialog.component.css']
+    styleUrls: ['./course-dialog.component.css'],
+    providers: [LoadingService]
 })
 export class CourseDialogComponent implements AfterViewInit {
 
@@ -40,7 +39,8 @@ export class CourseDialogComponent implements AfterViewInit {
 
     save() {
         const changes = this.form.value;
-        this.coursesService.saveCourse(this.course.id, changes).subscribe(
+        const saveCourse$ = this.coursesService.saveCourse(this.course.id, changes)
+        this.loadingService.showLoaderUntilComplited(saveCourse$).subscribe(
             (val) => {
                 this.dialogRef.close(val);
             }
--- src/app/loading/loading.service.ts
+++ src/app/loading/loading.service.ts
@@ -5,7 +5,6 @@ import { concatMap, finalize, tap } from "rxjs/operators";
 @Injectable()
 export class LoadingService {
     private loadingSubject = new BehaviorSubject<boolean>(false);
-
     loading$: Observable<boolean> = this.loadingSubject.asObservable();
 
     showLoaderUntilComplited<T>(obs$: Observable<T>): Observable<T> {
